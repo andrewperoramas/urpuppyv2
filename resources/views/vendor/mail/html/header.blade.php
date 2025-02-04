a8b52998e6bd30a07d8f315bcb51bd7de8a0b0ca
Andrew Peroramas <wolfandrew307@gmail.com>
2025-02-05 03:45:17 +0800

    FIX

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,8 +1,7 @@
-@props(['url'])
-<tr style="background-image: url('{{ imgconv64(public_path('images/email-template/banner-bg.png')) }}'); background-size: cover; background-position: center;">
+<tr style="background-image: url({{ asset('images/email-template/banner-bg.png') }}); background-size: cover; background-position: center;">
                         <td style="padding: 32px 24px;">
                             <a href="" style="display: inline-block; margin-bottom: 87px;">
-                                <img src="{{ imgconv64(public_path('images/email-template/Logo.png')) }}"
+                                <img src="{{ asset('images/email-template/Logo.png') }}"
                                     alt="logo" style="display: block;">
                             </a>
                             <h1
@@ -10,7 +9,6 @@
                                 Welcome to Urpuppy.com</h1>
                             <p
                                 style="font-family: Arial, Helvetica, sans-serif; font-size: 16px; font-weight: 400; line-height: 140%; color: #bbb9b9; margin: 0;">
-                                The Best Place to find Perfect Puppy Today! racih</p>
+                                The Best Place to find Perfect Puppy Today!</p>
                         </td>
                     </tr>
-
