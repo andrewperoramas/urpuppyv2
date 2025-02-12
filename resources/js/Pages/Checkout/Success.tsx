7c0f85fcf647772ac5d6d4e0d2514a614f89d040
Andrew Peroramas <wolfandrew307@gmail.com>
2025-02-12 22:03:06 +0800

    ADD

---
+++ resources/js/Pages/Checkout/Success.tsx
@@ -0,0 +1,18 @@
+import Layout from '@/Layouts/Layout'
+import React from 'react'
+
+const Success = () => {
+  return (
+  <Layout>
+        <section className="puppy-spotlight py-7 py-md-5 py-xl-9">
+            <div className="container">
+                    <div className="row mb-4 mb-lg-8">
+                       <h1>Success</h1>
+                    </div>
+           </div>
+        </section>
+    </Layout>
+  )
+}
+
+export default Success
