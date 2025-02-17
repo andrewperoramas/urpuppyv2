8d1c0f513b1705dab05e1a9228d725d2594638af
Andrew Peroramas <wolfandrew307@gmail.com>
2025-02-17 11:24:48 +0800

    AD

--- routes/web.php
+++ routes/web.php
@@ -81,7 +81,7 @@ Route::post('/create-intent', function (Request $request) {
 
 Route::get('adi', function () {
 
-    dd('updated samson');
+    dd('updated samson gervachis');
 });
 
 Route::post('/breeder/request/retry', function (Request $request) {
